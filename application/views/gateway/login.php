2f84da2bd1a4b2cce0f165ef6cc1e7a0239bf7c7
adgs-devteam <personal.mukti@gmail.com>
2022-03-05 19:44:58 +0700

    Commit di modul login.

--- application/views/gateway/login.php
+++ application/views/gateway/login.php
@@ -9,5 +9,8 @@
 <body>
 	halaman uji coba bentrok.
 	<p>Ini halaman yang ingin kita coba bentrokan dengan yang ada di master.</p>
+	<p>Tahap pertama melakukan perubahan di cabang modul_login.</p>
+	<p>Sekarang kita lakukanperubahan di cabang master juga.</p>
+	<p>Saat ini kita edit di halaman modul login.</p>
 </body>
 </html>
